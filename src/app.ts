6b39cbf928a14d6102af34d3c0dc6b4192c2a41a
Samuel Pinho A <devsamus@gmail.com>
2022-06-12 19:04:45 -0300

    setup inicial do express

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,17 @@
 //# 1 - inciando projeto 
-console.log("Express + Typescript!!!");
+//console.log("Express + Typescript!!!");
+
+//* 2 - init express
+
+import express from "express";
+
+const app = express()
+app.get("/",(req, res)=>{
+	return res.send("Hello Express!")
+})
+app.listen(3000, ()=> {
+	console.log("Aplicação de Typescript + Express funcionado");
+	console.log("http://localhost:3000/");
+	
+	
+})
