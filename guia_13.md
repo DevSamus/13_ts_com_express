3d7dd3306af44462eeebd4ecc95f2279253d518e
Samuel Pinho A <devsamus@gmail.com>
2022-06-13 18:20:13 -0300

    Interfaces Req e Res

--- guia_13.md
+++ guia_13.md
@@ -43,7 +43,23 @@
  175. Router parameters
   -> https://www.udemy.com/course/typescript-do-basico-ao-avancado-c-react-express/learn/lecture/30243986#overview
 
+176. Rotas mais complexas
+ -> https://www.udemy.com/course/typescript-do-basico-ao-avancado-c-react-express/learn/lecture/30243988#overview
 
+177. Router handler
+ ->  https://www.udemy.com/course/typescript-do-basico-ao-avancado-c-react-express/learn/lecture/30243990#overview
+
+178. Middlewares
+ -> https://www.udemy.com/course/typescript-do-basico-ao-avancado-c-react-express/learn/lecture/30243994#overview
+
+ 179. Middleware em todas as rotas
+ -> https://www.udemy.com/course/typescript-do-basico-ao-avancado-c-react-express/learn/lecture/30243998#overview
+
+ 180. Interfaces de Requisição e Resposta
+ -> https://www.udemy.com/course/typescript-do-basico-ao-avancado-c-react-express/learn/lecture/30244002#overview
+
+ 181. Tratando erros
+  -> https://www.udemy.com/course/typescript-do-basico-ao-avancado-c-react-express/learn/lecture/30246092#overview
 
 	TESTE DE APIS PARA VISUAL CODE COM - THUNDER CLIENT
   -> https://www.youtube.com/watch?v=dst-XewGjNo
--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,20 @@
 */
 
 //# 5 - interfaces do express
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+
 const app = express();
 
 //# 3 - Rota com POST
 app.use(express.json()); //* ativando um middleware
+
+//* 11 - Middleware para todas as rotas
+
+function showPath(req: Request, res: Response, next: NextFunction) {
+  console.log("Path", req.path);
+  next();
+}
+app.use(showPath);
 
 app.get("/", (req, res) => {
   return res.send("Hello Express!");
@@ -53,6 +62,71 @@ app.get("/api/json", (req: Request, res: Response) => {
   });
 });
 
+//# 7 - router parameters
+
+app.get("/api/product/:id", (req: Request, res: Response) => {
+  console.log(req.params);
+  const id = req.params.id;
+  if (id === "1") {
+    const product = {
+      id: 1,
+      name: "Boné",
+      price: 10.99,
+    };
+    return res.json(product);
+  }
+
+  return res.send("Produto não encontrado!!");
+});
+
+//* 8 - Rotas Complexas
+
+app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
+  console.log(req.params);
+  const productId = req.params.id;
+  const reviewId = req.params.reviewId;
+  return res.send(`Acessando a review ${reviewId} do produto ${productId}`);
+});
+
+//# 9 - router handler
+
+function getUser(req: Request, res: Response) {
+  console.log(`Usuário ID: ${req.params.id}`);
+  return res.send("Usuário encontrado!!");
+}
+
+app.get("/api/user/:id", getUser);
+
+//* 10 - middleware
+
+function checkUser(req: Request, res: Response, next: NextFunction) {
+  if (req.params.id === "1") {
+    console.log("Pode seguir!!");
+    next();
+    return;
+  }
+  console.log("Acesso restrito");
+  return res.json({ err: "Acesso restrito" });
+}
+
+app.get("/api/user/:id/access", checkUser, (req: Request, res: Response) => {
+  return res.json({ msg: "Bem-vindo a área administrativa!" });
+});
+
+//# 12 - req e res com generics
+
+app.get("/api/user/:id/details/:name",
+  (
+    req: Request<{ id: string; name: string }>,
+    res: Response<{ status: boolean }>
+  ) => {
+		console.log(`Id: ${req.params.id}`);
+		console.log(`Name: ${req.params.name}`);
+
+		return res.json({status:true})
+	}
+);
+
 app.listen(3000, () => {
   console.log("Aplicação de Typescript + Express funcionado");
   console.log("http://localhost:3000/");
@@ -60,4 +134,9 @@ app.listen(3000, () => {
   console.log("http://localhost:3000/api/product/check");
   console.log("http://localhost:3000/api/interfaces");
   console.log("http://localhost:3000/api/json");
+  console.log("http://localhost:3000/api/product/:id");
+  console.log("http://localhost:3000/api/product/:id/review/:reviewId");
+  console.log("http://localhost:3000/api/user/:id");
+  console.log("http://localhost:3000/api/user/:id/access");
+  console.log("http://localhost:3000/api/user/12/details/James");
 });
